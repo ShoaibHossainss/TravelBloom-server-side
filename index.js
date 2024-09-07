306a22a60576f9b02256bddadd4da1ae1a5e8df7
Shovanss <imshovanss@gmail.com>
2024-09-07 17:24:27 +0600

    single tourist spot added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dr9an2m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +34,28 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+const touristSpotCollection = client.db("touristSpotDB").collection("touristSpot");
+
+
+app.get('/touristSpot',async(req,res)=>{
+  const cursor = touristSpotCollection.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+app.get('/touristSpot/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await touristSpotCollection.findOne(query);
+  res.send(result);
+})
+app.post('/touristSpot',async(req,res)=>{
+  const item = req.body;
+  const result = await touristSpotCollection.insertOne(item);
+  res.send(result);
+})
+
+
 
 
 
